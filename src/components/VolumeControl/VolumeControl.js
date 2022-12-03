eb52d93f1fa62e3bdd0fa9dd18e86e03210f61ae
t1pp1 <pengzouyiyun@gmail.com>
2022-12-03 11:41:41 -0500

    feat: add volume icon

--- src/components/VolumeControl/VolumeControl.js
+++ src/components/VolumeControl/VolumeControl.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useStateProvider } from "../../utils/StateProvider";
 import "./VolumeControl.css";
-
+import {
+  RiVolumeMuteLine,
+  RiVolumeDownLine,
+  RiVolumeUpLine,
+} from "react-icons/ri";
 /**
  * this component is the volume slider on the right side of the footer
  * @returns the volume slider
  */
 export default function VolumeControl() {
   const [{ token }] = useStateProvider();
+  const [currentVolume, setCurrentVolume] = useState(50);
   const setVolume = async (e) => {
     let newVolume = parseInt(e.target.value);
+    setCurrentVolume(newVolume)
     await axios.put(
       "https://api.spotify.com/v1/me/player/volume",
       {},
@@ -27,7 +33,10 @@ export default function VolumeControl() {
   };
   return (
     <div className="volumeControl">
-      <input type="range" onMouseUp={(e) => setVolume(e)} min={0} max={100} />
+      {currentVolume ? <>
+      {currentVolume > 50 ? <RiVolumeUpLine id="volumeIcon" /> : <RiVolumeDownLine id="volumeIcon"/>}
+      </> : <RiVolumeMuteLine id="volumeIcon"/>}
+      <div><input type="range" onMouseUp={(e) => setVolume(e)} min={0} max={100} /></div>
     </div>
   );
 }
